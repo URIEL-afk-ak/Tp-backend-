41b52973622027154ad995fa6948ad2b5c570c9e
PedroYanover <>
2025-11-25 17:48:33 -0300

    anda

--- transport - copia (2)/ms-facturacion-v2/src/main/java/com/tpi/facturacion/exception/FacturaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tpi.facturacion.exception;
-
-public class FacturaNotFoundException extends RuntimeException {
-    public FacturaNotFoundException(String message) {
-        super(message);
-    }
-}
